4ca39142f1584f47733c38b28894853c8da797f3
tassaron <rutachigal@gmail.com>
2022-03-30 16:56:49 -0400

    pipes come from the pipe trough!

---
+++ src/scenes/PipeTroughScene.js
@@ -0,0 +1,40 @@
+import * as PIXI from "pixi.js";
+import Scene from "muffin-game/scenes/Scene";
+import { logger } from "muffin-game/core/logger";
+
+
+export default class PipeTroughScene extends Scene {
+    subcontainer = new PIXI.Container();
+
+    constructor(game) {
+        super(game);
+        this.subcontainer.y = game.height - 73;
+        this.upcomingPipes = [];
+        for (let i = 0; i < 5; i++) {
+            this.addPipeToTrough();
+        }
+    }
+
+    getNextPipe() {
+        const nextPipe = this.upcomingPipes.splice(0, 1);
+        logger.info(`Removing ${nextPipe} from trough scene's container`)
+        if (this.mounted) {
+            this.mounted.removeChild(this.actors[nextPipe]);
+        }
+        this.addPipeToTrough();
+        return this.actors[nextPipe];
+    }
+
+    addPipeToTrough() {
+        const i = 4;
+        const newPipe = this.game.sprites.pipe();
+        const pipeId = this.addActors([newPipe])[0];
+        this.subcontainer.addChild(newPipe);
+        for (let j = 0; j < this.upcomingPipes.length; j++) {
+            this.actors[this.upcomingPipes[j]].x = j * 73;
+        }
+        newPipe.x = 73 * i;
+        logger.info(`Registered new pipe in trough: ${pipeId}`);
+        this.upcomingPipes.push(pipeId);
+    }
+}
--- src/scenes/WorldScene.js
+++ src/scenes/WorldScene.js
@@ -1,27 +1,50 @@
 import * as PIXI from "pixi.js";
-import Scene from "muffin-game/scenes/Scene";
+import { logger } from "muffin-game/core/logger";
 import { newBackButton } from "muffin-game/scenes/MenuScene";
+import Scene from "muffin-game/scenes/Scene";
 import PipeGridScene from "./PipeGridScene";
+import PipeTroughScene from "./PipeTroughScene";
 import MyMenuScene from "./MenuScene";
 
 
 export default class WorldScene extends Scene {
+    levelTimers = [480.0, 240.0, 120.0];
+    level = 0;
+    timer = -1;
+
     constructor(game) {
         super(game);
         this.actors.backButton = newBackButton(game, MyMenuScene);
         this.grid = new PipeGridScene(game);
+
+        this.trough = new PipeTroughScene(game);
         for (let x = 0; x < this.grid.cols; x++) {
             for (let y = 0; y < this.grid.rows; y++) {
                 this.grid._grid[y][x].interactive = true;
                 this.grid._grid[y][x].pointertap = (e) => {
                     if (!this.grid.mounted) return;
-                    const pipe = game.sprites.pipe();
+                    const pipe = this.trough.getNextPipe();
                     this.grid._grid[y][x].addChild(pipe);
-                    this.grid.addActors([pipe]);
                     this.grid._grid[y][x].interactive = false;
+                    pipe.y = 0;
+                    pipe.x = 0;
                 };
             }
         }
-        this.subscenes = [this.grid];
+        this.beforeMount(() => this.startWaterTimer());
+        this.subscenes = [this.grid, this.trough];
+    }
+
+    startWaterTimer() {
+        this.timer = this.game.startTimer(
+            () => {
+                this.grid.startWater();
+                this.timer = -1;
+            },
+            this.levelTimers[this.level]
+        );
+        this.beforeUnmount(() => {
+            if (this.timer > -1) this.game.stopTimer(this.timer)
+        });
     }
 }
