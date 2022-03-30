940cd3b2bee3e7cccce5014ce74176fc5c860190
tassaron <rutachigal@gmail.com>
2022-03-30 19:33:01 -0400

    1) flow animation for water source, 2) recalculate direction at walls

--- src/actors/WaterSourceTileActor.js
+++ src/actors/WaterSourceTileActor.js
@@ -1,16 +1,33 @@
+import { logger } from "muffin-game/core/logger";
 import RectangleActor from "muffin-game/actors/RectangleActor";
+import EllipseActor from "muffin-game/actors/EllipseActor";
+
+
+export const randomDirection = () => Math.floor(Math.random() * 4);
 
 
 export default class WaterSourceTileActor extends RectangleActor {
-    direction = Math.floor(Math.random() * 4);
+    flowAnimation = null;
+    direction = randomDirection();
     /* 0 - left
      * 1 - right
      * 2 - down
      * 3 - up
      */
 
-    constructor(game) {
+    constructor(game, coords, maxY, maxX) {
         super(game, 73, 73, 0x000000, 0xffffff);
+
+        const [y, x] = coords;
+        logger.debug(`Water source placed at x${x},y${y}. MaxX is ${maxX}, maxY is ${maxY}`);
+        while (
+            (x == 0 && this.direction == 0) ||
+            (x == maxX && this.direction == 1) ||
+            (y == maxY && this.direction == 2) ||
+            (y == 0 && this.direction == 3)) {
+                logger.debug("Oops! Water source tile had to change direction");
+                this.direction = randomDirection();
+        }
 
         // Overwrite the outline in the direction water will flow from
         const createFlowIndicator = () => {
@@ -35,5 +52,23 @@ export default class WaterSourceTileActor extends RectangleActor {
             }
         };
         this.flowIndicator = createFlowIndicator();
+    }
+
+    startFlowAnimation() {
+        this.flowAnimation = new EllipseActor(this.game, 1, 1, 0x0000ff, null);
+        this.addChild(this.flowAnimation);
+        this.flowAnimation.x = 73 / 2;
+        this.flowAnimation.y = 73 / 2;
+        this.game.scene.beforeTick((delta, keyboard) => {
+            if (!this.flowAnimation) return;
+            this.flowAnimation.x -= delta / 8;
+            this.flowAnimation.y -= delta / 8;
+            this.flowAnimation.width += delta / 4;
+            this.flowAnimation.height += delta / 4;
+        });
+    }
+
+    stopFlowAnimation() {
+        this.flowAnimation = null;
     }
 }
