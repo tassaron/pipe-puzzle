339491d1738d7664dd56cf86785988564585e667
tassaron <rutachigal@gmail.com>
2022-03-31 14:35:36 -0400

    might as well save the x and y in every tile actor

--- src/actors/EmptyPipeTileActor.js
+++ src/actors/EmptyPipeTileActor.js
@@ -3,7 +3,8 @@ import RectangleActor from "muffin-game/actors/RectangleActor";
 
 export default class EmptyPipeTileActor extends RectangleActor {
     constructor(game, x, y) {
-        // not using x and y yet but probably will someday
         super(game, 73, 73, 0xffffff, 0x000000);
+        this.gridx = x;
+        this.gridy = y;
     }
 }
