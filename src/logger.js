85bdc7cd5af3000e6bec810ab15ceb24572f47de
tassaron <rutachigal@gmail.com>
2022-03-31 23:21:52 -0400

    raise logger level in git so I don't forget in production later

--- src/logger.js
+++ src/logger.js
@@ -2,5 +2,5 @@ import Logger, { logger as engine_logger } from "muffin-game/core/logger";
 
 
 export const logger = new Logger("pipe-puzzle");
-logger.minimum = logger.level.info;
+logger.minimum = logger.level.error;
 engine_logger.minimum = logger.level.error;
