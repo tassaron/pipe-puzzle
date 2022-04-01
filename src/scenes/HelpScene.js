4fbc8a3c3befb697104b5031585884e55bab71c2
tassaron <rutachigal@gmail.com>
2022-03-31 21:37:52 -0400

    helpful

--- src/scenes/HelpScene.js
+++ src/scenes/HelpScene.js
@@ -1,11 +1,18 @@
 import Scene from "muffin-game/scenes/Scene";
 import { newBackButton } from "muffin-game/scenes/MenuScene";
-import MyMenuScene from "./MenuScene";
+import MenuScene from "./MenuScene";
+import EllipseActor from "muffin-game/actors/EllipseActor";
+import ButtonActor from "muffin-game/actors/ButtonActor";
 
 
 export default class HelpScene extends Scene {
     constructor(game) {
         super(game);
-        this.actors.backButton = newBackButton(game, MyMenuScene);
+        this.actors.backButton = newBackButton(game, MenuScene);
+        this.actors.helpText = new ButtonActor(game, EllipseActor, 325, 125, "water should flow!", {fontSize: "48px"}, 0x2187d9, null);
+        this.actors.helpText.anchor.x = 0.5;
+        this.actors.helpText.anchor.y = 0.5;
+        this.actors.helpText.x = game.width / 2;
+        this.actors.helpText.y = game.height / 2;
     }
 }
