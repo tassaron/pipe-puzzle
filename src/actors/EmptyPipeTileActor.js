76500123d97c8e5832342c856e553133ced9cd10
tassaron <rutachigal@gmail.com>
2022-04-04 21:02:12 -0400

    change grid colour, reduce wiggle when clicking tile

--- src/actors/EmptyPipeTileActor.js
+++ src/actors/EmptyPipeTileActor.js
@@ -4,10 +4,10 @@ import WiggleAction from "muffin-game/actors/actions/WiggleAction";
 
 export default class EmptyPipeTileActor extends RectangleActor {
     constructor(game, x, y) {
-        super(game, 73, 73, 0xffffff, 0x3366aa);
+        super(game, 73, 73, 0xffffff, 0xafc3de);
         this.gridx = x;
         this.gridy = y;
-        this._wiggle = new WiggleAction(game, this);
+        this._wiggle = new WiggleAction(game, this, 2);
         this._wiggle.direction = 2;
         this.pivot.x = 73 / 2;
         this.pivot.y = 73 / 2;
